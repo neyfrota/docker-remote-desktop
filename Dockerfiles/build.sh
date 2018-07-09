e69050a173a64b60f673df33d46623acd8d83e9e
ney frota <ney@frota.net>
2018-07-09 10:53:58 -0300

    Add resilio sync and other basic apps

--- Dockerfiles/build.sh
+++ Dockerfiles/build.sh
@@ -3,17 +3,31 @@ echo "==============================================="
 echo "docker build"
 echo "==============================================="
 
-echo "Update system"
+echo "Update/upgrade system"
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get upgrade -y
 
-echo "Install packages"
+echo "Install basic tools"
 apt-get install -y software-properties-common openssh-server dnsutils curl sudo vim net-tools dconf-cli
+
+echo "Add extra repository"
 apt-add-repository -y ppa:ubuntu-mate-dev/ppa
 apt-add-repository -y ppa:ubuntu-mate-dev/trusty-mate
+add-apt-repository -y ppa:webupd8team/atom
+echo "deb http://linux-packages.resilio.com/resilio-sync/deb resilio-sync non-free" | sudo tee /etc/apt/sources.list.d/resilio-sync.list
+wget -qO - https://linux-packages.resilio.com/resilio-sync/key.asc | sudo apt-key add -
+
+echo "Update"
 apt-get update -y
-apt-get install -y vnc4server mate-desktop-environment
+
+echo "Install packages"
+apt-get install -y vnc4server mate-desktop-environment firefox resilio-sync nicotine transmission baobab vlc 
+# this one fail :(  atom pinta
+
+echo "Post-install actions"
+sudo systemctl stop resilio-sync
+sudo systemctl disable resilio-sync
 
 echo "Clean system"
 apt-get autoremove -y
--- Dockerfiles/entrypoint.sh
+++ Dockerfiles/entrypoint.sh
@@ -4,10 +4,7 @@ echo "docker entrypoint"
 echo "==============================================="
 
 
-
-# -----------------------
-# Cleanup env variables
-# -----------------------
+echo "Set variables"
 export uid=${uid:-1000}
 export gid=${gid:-1000}
 export group=${group:=user}
@@ -17,10 +14,6 @@ export resolution=${resolution:=1024x768}
 # todo: validate env to avoid hack
 
 
-
-# -----------------------
-# add user
-# -----------------------
 echo "Create user $username:$password ($uid)"
 echo "Create group $group ($gid)"
 groupadd --gid $gid $group
@@ -50,12 +43,7 @@ else
 fi
 
 
-
-
-# -----------------------
-# start  vnc
-# -----------------------
-echo "Start vnc with resolution $resolution"
+echo "Start local vnc with resolution $resolution"
 mkdir -p /home/$username/.vnc
 rm -f /home/$username/.vnc/passwd
 rm -f /home/$username/.vnc/xstartup
@@ -81,19 +69,41 @@ pkill Xvnc4
 su $username -c "vncserver :0 -name desktop -nolisten tcp -localhost -geometry $resolution -depth 24 -SecurityTypes none "
 
 
+echo "Start rslsync at port :8888"
+mkdir /home/$username/.rslsync 2>/dev/null
+mkdir /home/$username/.rslsync/storage 2>/dev/null
+echo "{
+    \"listening_port\" : 0,
+    \"storage_path\" : \"/home/$username/.rslsync/storage/\",
+    \"pid_file\" : \"/home/$username/.rslsync/rslsync.pid\",
+    \"agree_to_EULA\": \"yes\",
+    \"device_name\": \"remote-desktop\",
+    \"check_for_updates\" : false,
+    \"use_upnp\" : true,
+	\"directory_root\" : \"/home/$username/\",
+    \"webui\" :
+    {
+        \"listen\" : \"127.0.0.1:8888\"
+    }
+}" > /home/$username/.rslsync/config.json
+chown -Rf $username.$group  /home/$username/.rslsync/
+chmod a-rwx,u+rwX /home/$username/.rslsync
+chmod a-rwx,u+rwX /home/$username/.rslsync/storage
+chmod a-rwx,u+rwX /home/$username/.rslsync/config.json
+pkill rslsync
+sleep 2
+su $username -c "/usr/bin/rslsync --config /home/$username/.rslsync/config.json"
 
-# -----------------------
-# start ssh
-# -----------------------
-echo "Start ssh at port 22"
+
+
+
+
+echo "Start ssh at port :22"
 /etc/init.d/ssh stop
 /etc/init.d/ssh start
 
 
 
-# -----------------------
-# stay up
-# -----------------------
 echo "Holding instance up."
 while :; do
   date
